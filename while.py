8067d3fccfd48626ba68f9963708b0119d47f671
Azaki <zakidawud69@gmail.com>
2025-10-30 21:35:29 +0700

    Dasar

--- while.py
+++ while.py
@@ -1,4 +1,16 @@
-angka = 1
-while angka <= 5:
-    print(angka)
-    angka += 1
+password_true = "etcode99"
+percobaan = 0
+max_percobaan = 3
+
+while percobaan < max_percobaan:
+    password_input = input("Masukkan password: ")
+
+    if password_input == password_true:
+        print("Akses diterima.")
+        break
+    else:
+        percobaan += 1
+        print(f"Password salah. Percobaan ke-{percobaan} dari {max_percobaan}.")
+
+else:
+    print("Akses ditolak. Terlalu banyak percobaan gagal.")
