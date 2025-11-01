56f01fe522fa4902de6c41dff0b420c5996d016c
Azaki <zakidawud69@gmail.com>
2025-11-01 23:43:16 +0700

    Dasar

---
+++ module.py
@@ -0,0 +1,14 @@
+# import function_m as f
+
+# sapa = f.sapa("Zaki")
+# total = f.total(10,20,30,40,50)
+# print(sapa)
+# print(total)
+
+from function_m import sapa
+from function_m import total
+
+sapa("Bill")
+print(total(15, 25, 35, 45, 55))
+
+# Program ini menggunakan import module untuk mengakses fungsi dalam file function_m.py
